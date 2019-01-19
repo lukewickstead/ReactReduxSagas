010089437c74c7951171782f93f0352dc1d59c31
lwickstead <lwickstead@mubaloo.com>
2019-01-19 13:32:05 +0000

    Adding in basic loading, error  and imageWithDescription components.

--- src/api/nasaApis.js
+++ src/api/nasaApis.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export function getAstronomyPictureOfTheDay() {
-    return axios.get(`https://api.nasa.gov/planetary/apod?api_key=FixAPIKey`);
+    return axios.get(`https://api.nasa.gov/planetary/apod?api_key=CeJ9zuuspOQx0OLbP6xVZRElindbAoXdfYSyDWpx`);
 }
